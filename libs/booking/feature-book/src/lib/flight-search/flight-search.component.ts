d4b67b478dd7c7c073b2dfbc59f03dfb77a602e2
mikezks <23002661+mikezks@users.noreply.github.com>
2023-03-22 21:28:56 +0100

    feat: flight search signals

--- libs/booking/feature-book/src/lib/flight-search/flight-search.component.ts
+++ libs/booking/feature-book/src/lib/flight-search/flight-search.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject, Inject } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { Store } from "@ngrx/store";
-import { BookingSlice, delayFlight, loadFlights, selectFlights } from "@nx-example/booking/domain";
+import { BookingSlice, delayFlight, Flight, loadFlights, selectFlights } from "@nx-example/booking/domain";
+import { fromObservable, injectSignalChangeDetection, signal } from '@nx-example/shared/util-signals';
 import { take } from "rxjs";
+import { SettableSignal } from './../../../../../shared/util-signals/src/lib/angular/core/signals/src/signal';
 
 import { FlightCardComponent } from '@nx-example/booking/ui-common';
 import { CityValidator } from '@nx-example/shared/util-common';
@@ -11,8 +13,8 @@ import { CityValidator } from '@nx-example/shared/util-common';
 @Component({
   standalone: true,
   imports: [
-    CommonModule, 
-    FormsModule, 
+    CommonModule,
+    FormsModule,
     FlightCardComponent,
     CityValidator,
   ],
@@ -22,34 +24,36 @@ import { CityValidator } from '@nx-example/shared/util-common';
   templateUrl: './flight-search.component.html'
 })
 export class FlightSearchComponent {
-
-  from = 'Berlin'; 
-  to = 'London'; 
-  urgent = false;
-
   store = inject(Store<BookingSlice>);
-  flights$ = this.store.select(selectFlights);
 
-  basket: { [id: number]: boolean } = {
-    3: true,
-    5: true
-  };
+  state = injectSignalChangeDetection({
+    from: signal('Berlin'),
+    to: signal('London'),
+    urgent: signal(false),
+    basket: signal<Record<number, boolean>>({
+      3: true,
+      5: true
+    }),
+    flights: fromObservable(this.store.select(selectFlights)) as SettableSignal<Flight[]>
+  });
 
   search(): void {
-    if (!this.from || !this.to) return;
+    if (!this.state.from() || !this.state.to()) return;
 
     this.store.dispatch(loadFlights({
-      from: this.from, 
-      to: this.to 
+      from: this.state.from(),
+      to: this.state.to()
     }));
   }
 
   delay(): void {
-    this.flights$.pipe(take(1)).subscribe(flights => {
-      const id = flights[0].id;
-      this.store.dispatch(delayFlight({id}));
-    });
+    this.store.dispatch(delayFlight({
+      id: this.state.flights()[0].id
+    }));
   }
 
+  updateBasket(id: number, selected: boolean): void {
+    this.state.basket.mutate(basket => basket[id] = selected);
+  }
 }
 
